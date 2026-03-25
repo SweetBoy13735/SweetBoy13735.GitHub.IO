250e6af0daf0d293139d38b20c6a638750e2e7de
Ramone Graham <ramonegraham@gmail.com>
2026-03-25 22:25:27 +0000

    Install Babel for React
    Installed Babel and its related presets, and modified the Webpack config to allow JSX compilation.

--- webpack.common.js
+++ webpack.common.js
@@ -9,6 +9,15 @@ module.exports = {
 		path: Path.resolve(__dirname, "dist"),
 		clean: true,
 		filename: "./res/js/app.js",
-	}
+	}, module: {
+		rules: [{
+				test: /\.(m?js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+				}
+		}]
+	}, resolve: { extensions: ["...", ".jsx"] }
 };
 //#endregion
