ccdb03fac1821daf331322c4330696ca514f44f1
Ramone Graham <ramonegraham@gmail.com>
2026-06-14 02:26:41 +0100

    Install testing libraries
    Installed Jest, React Testing Library and helper modules required to run my test suite.

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+//#region Code body
+/** @type {import("jest").Config} */
+const config = { testEnvironment: "jsdom" };
+//#endregion
+
+//#region Module exports
+export default config;
+//#endregion
