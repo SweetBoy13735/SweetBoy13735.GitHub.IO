dd44caf932bd3327f61d190cc7031aff2beab2c9
Ramone Graham <ramonegraham@gmail.com>
2026-06-23 04:18:04 +0100

    Implement component rendering
    React components will now render on the app root. I've also created a launch config for starting a webpack development server.

---
+++ public/res/js/index.js
@@ -0,0 +1,16 @@
+//#region External module imports
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+//#endregion
+
+//#region Internal module imports
+import App from "./components/App";
+//#endregion
+
+//#region Code body
+const appRoot = createRoot(document.getElementById("app-root") || document.body);
+
+appRoot.render(<StrictMode>
+	<App />
+</StrictMode>);
+//#endregion
